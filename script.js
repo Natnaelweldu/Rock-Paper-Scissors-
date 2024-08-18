316d30dfb1e102043494ec1fe0ff4c554f714918
Natnael Woldu <natnael.Welduu@gmail.com>
2024-08-18 17:08:52 +0300

    Create the getComputerChoice function
    get the choice of the computer randomly

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3);
+  let choice;
+  switch (random) {
+    case 0:
+      choice = "Rock";
+      break;
+    case 1:
+      choice = "Paper";
+      break;
+    case 2:
+      choice = "Scissor";
+      break;
+  }
+  return choice;
+}
